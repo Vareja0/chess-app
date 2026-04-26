9ac4d1d61933ff58105857e53dbe490da6f552ec
Riscala Neto <163042408+Vareja0@users.noreply.github.com>
2026-04-25 23:15:22 -0300

    Remove docker-compose.prod.yml in before_install script
    Remove existing docker-compose.prod.yml file before installation.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,5 +2,6 @@
 # before_install.sh
 set -e
 systemctl stop chess-app 2>/dev/null || true
+rm -f /opt/chess-app/docker-compose.prod.yml
 mkdir -p /opt/chess-app /var/log/chess-app
 chown -R ec2-user:ec2-user /opt/chess-app /var/log/chess-app
