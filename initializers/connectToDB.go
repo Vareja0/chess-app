121eb9044e5e88e462bfb5ef66714abcbba4b53e
Vareja0 <netomiguelfadel@gmail.com>
2026-05-03 12:57:25 -0300

    fix: enable SSL for RDS connection and improve error logging

--- initializers/connectToDB.go
+++ initializers/connectToDB.go
@@ -1,6 +1,7 @@
 package initializers
 
 import (
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -12,10 +13,10 @@ var DB *gorm.DB
 func ConnectDb() {
 	var err error
 	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") +
-		" dbname=" + os.Getenv("DB_NAME") + " port=5432 sslmode=disable"
+		" dbname=" + os.Getenv("DB_NAME") + " port=5432 sslmode=require"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		panic("Failed to connect to db")
+		log.Fatalf("failed to connect to db: %v", err)
 	}
 }
